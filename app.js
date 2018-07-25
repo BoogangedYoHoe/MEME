d8d9b6727a19a5aa665c8e1263920df0fd17d125
unknown <kvashmar@yahoo.com>
2018-07-24 19:11:02 -0700

    added new memes, added JS button w/ audio and video

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('hello world');
+
+// Opens jQuery
+$(document).ready(function(){
+  var $jumpscare = $('#jumpscare');
+  var $jumpaudio = $('#jump-audio')[0]; // [0] for multiple sources
+  $jumpscare.hide();
+
+  $('#jump-button').on('click', function(){
+    $jumpscare.show();
+    $jumpaudio.play();
+    setTimeout(function(){ $jumpscare.hide(); } , 2000);
+  })
+
+}); // closes jQuery
