4a725f4ddc83aded84902e628f51647729411e75
unknown <kvashmar@yahoo.com>
2018-07-27 14:34:38 -0700

    rebuilding after a git revert

--- app.js
+++ app.js
@@ -3,23 +3,37 @@ console.log('hello world');
 // Opens jQuery
 $(document).ready(function(){
   var $jumpscare = $('#jumpscare');
-  var $jumpaudio = $('#jump-audio')[0]; // [0] for multiple sources
+  var $jumpaudio = $('#jump-audio')[0]; // [0] for selcting between multiple sources: [0]=first source, [1]=second source
   $jumpscare.hide();
-
-  // $("#bgmusic")[0].play();
 
   $('#jump-button').on('click', function(){
     $jumpscare.show();
     $jumpaudio.play();
-    setTimeout(function(){ $jumpscare.hide(); } , 2000);
+    setTimeout(function(){ $jumpscare.hide(); } , 2000); // 2000 = 2s
   })
-  $("button").click(function(){
-      $("#div1").load("demo_test.txt", function(responseTxt, statusTxt, xhr){
-          if(statusTxt == "success")
-              alert("External content loaded successfully!");
-          if(statusTxt == "error")
-              alert("Error: " + xhr.status + ": " + xhr.statusText);
-      });
-  });
+
+  // Hide sections on page load
+  $('#drawings').hide();
+  $('#videos').hide();
+
+  // Menu Controls
+  $('#menu-memes').click(function(){
+    console.log('memes');
+    $('#memes').show();
+    $('#drawings').hide();
+    $('#videos').hide();
+  })
+  $('#menu-drawings').click(function(){
+    console.log('drawings');
+    $('#memes').hide();
+    $('#drawings').show();
+    $('#videos').hide();
+  })
+  $('#menu-videos').click(function(){
+    console.log('videos');
+    $('#memes').hide();
+    $('#drawings').hide();
+    $('#videos').show();
+  })
 
 }); // closes jQuery
